1fbf266b3fa63e632925d18edc7eded77b6643cd
TZK- <contact@tzk.fr>
2017-10-20 10:35:39 +0200

    Init

---
+++ Assets/Scripts/EntityController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector3(moveHorizontal, moveVertical);
+        Vector3 rotation = new Vector3(0, 0, 0);
+
+        transform.Translate(movement * speed);
+
+        //Fixe la rotation
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
